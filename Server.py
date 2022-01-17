9f57f2d843aa1a10f88d2fb13b060578072ed83a
mateusz-stelmasiak <46268673+mateusz-stelmasiak@users.noreply.github.com>
2022-01-17 23:38:39 +0100

    Function rename + print msg once

--- Server.py
+++ Server.py
@@ -1,17 +1,17 @@
 import socket
-
-IPw = input("Podaj IP serwera: ")
+#curr IP 192.168.137.17
+IPw = input("Set server IP: ")
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # utworzenie gniazda
-serversocket.bind((IPw, 8070))  # dowiazanie do portu 1234
+serversocket.bind((IPw, 8070))
 serversocket.listen(5)
 savedPasswords = []
 
 
-def listentostatus():
+def listen_to_status():
     return str(clientSocket.recv(2048), 'utf-8')
 
 
-def savepasswordtoserver(password: str):
+def save_password_to_server(password: str):
     try:
         savedPasswords.append(password)
         return "Success"
@@ -19,7 +19,7 @@ def savepasswordtoserver(password: str):
         return "Something went wrong"
 
 
-def checkPassword(password: str):
+def check_password(password: str):
     for passw in savedPasswords:
         if passw == password:
             return "Success"
@@ -28,25 +28,35 @@ def checkPassword(password: str):
 
 def authorize(matchScore):
     if float(matchScore) > 0.5:
-        print("success")
+        print("Auth success")
     else:
         print("Auth failed")
 
 
-def setNewPassword(clientSocket):
-    clientSocket.send("setPassword")
+def set_client_mode_new_password(clientSocket):
+    print("sending...")
+    set_password_as_bytes = str.encode("setPassword")
+    clientSocket.send(set_password_as_bytes)
 
 
-def decodeMessage(message):
-    print(message)
+def decode_message(message):
     if message[0] == "%":
         authorize(message[1:])
+    if message[0] == "$":
+        save_password_to_server(message)
     else:
-        print(message)
+        print("Received: "+message)
 
 
-print("Nasluchiwanie: ")
+def interpret_command(command,clientSocket):
+    if command == "n":
+        set_client_mode_new_password(clientSocket)
+
+
+
+print("Listening for connections...")
+# print("commands: n- new password|")
 while 1:
     clientSocket, address = serversocket.accept()
-    rec = listentostatus()
-    decodeMessage(rec)
+    rec = listen_to_status()
+    decode_message(rec)
