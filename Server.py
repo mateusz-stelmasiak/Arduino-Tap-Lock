99d7e63f29b91bf053df64f84c36575f158f4c67
Wojciech Mazurowski <50964241+Wojciech-Mazurowski@users.noreply.github.com>
2022-01-17 20:57:32 +0100

    server update

--- Server.py
+++ Server.py
@@ -4,11 +4,26 @@ IPw = input("Podaj IP serwera: ")
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # utworzenie gniazda
 serversocket.bind((IPw, 8070))  # dowiazanie do portu 1234
 serversocket.listen(5)
+savedPasswords = []
 
 
 def listentostatus():
     return str(clientSocket.recv(1024), 'utf-8')
 
+
+def savepasswordtoserver(password: str):
+    try:
+        savedPasswords.append(password)
+        return "Success"
+    except:
+        return "Something went wrong"
+
+
+def checkPassword(password: str):
+    for passw in savedPasswords:
+        if passw == password:
+            return "Success"
+    return "Wrong password"
 
 def authorize(matchScore):
     if float(matchScore) > 0.5:
